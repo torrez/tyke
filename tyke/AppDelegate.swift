2313e06aead626a3c1a42bb2bddd7088bf4509f2
Andre Torrez <andre@torrez.org>
2017-08-17 14:11:53 -0700

    Initial Commit

---
+++ tyke/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  tyke
+//
+//  Created by Andre Torrez on 8/17/17.
+//  Copyright © 2017 Andre Torrez. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
